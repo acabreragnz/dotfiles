cf3f85557085e14958ea156de9e3d2fa862b2c91
Toni Cabrera <acabreragnz@gmail.com>
2026-04-14 23:41:26 -0300

    Add gws-login.sh script for Google Workspace re-authentication

---
+++ scripts/gws-login.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gws auth login --account acabreragnz@gmail.com --scopes "https://www.googleapis.com/auth/drive,https://mail.google.com/,https://www.googleapis.com/auth/spreadsheets,https://www.googleapis.com/auth/calendar,https://www.googleapis.com/auth/documents,https://www.googleapis.com/auth/tasks"
