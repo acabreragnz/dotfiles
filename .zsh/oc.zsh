0cd380506e0353451e77a65f3ee31d23de5dc4e0
Toni Cabrera <acabreragnz@gmail.com>
2026-05-06 13:07:39 -0300

    rename opencode yolo wrapper to ocd

--- .zsh/oc.zsh
+++ .zsh/oc.zsh
@@ -4,7 +4,7 @@ oc() {
   command opencode "$@"
 }
 
-ocy() {
+ocd() {
   # Match the proposed YOLO mode once the installed opencode version supports it.
   local PATH="$HOME/.bun/bin:$HOME/.local/kitty.app/bin:$HOME/.local/bin:$HOME/.opencode/bin:$HOME/.config/composer/vendor/bin:$HOME/.local/share/pnpm:$HOME/.console-ninja/.bin:$HOME/.asdf/shims:/home/linuxbrew/.linuxbrew/opt/asdf/libexec/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$HOME/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
   local help_text version
@@ -16,6 +16,6 @@ ocy() {
   fi
 
   version=$(command opencode --version 2>/dev/null)
-  print -u2 "ocy: opencode ${version:-unknown} todavia no soporta --dangerously-skip-permissions; usando OPENCODE_DANGEROUSLY_SKIP_PERMISSIONS=true por compatibilidad futura."
+  print -u2 "ocd: opencode ${version:-unknown} todavia no soporta --dangerously-skip-permissions; usando OPENCODE_DANGEROUSLY_SKIP_PERMISSIONS=true por compatibilidad futura."
   OPENCODE_DANGEROUSLY_SKIP_PERMISSIONS=true command opencode "$@"
 }
